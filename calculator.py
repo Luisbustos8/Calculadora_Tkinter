b6c77dcc861de7b5caa529f541addedcdf24114e
Luis Alejandro Bustos Lucas <Luis@MacBook-Pro-de-Luis-5.local>
2020-05-21 22:27:40 +0200

    Pinta teclado y cambia con selector

--- calculator.py
+++ calculator.py
@@ -184,8 +184,11 @@ class Controlator(ttk.Frame):
         self.display = Display(self)
         self.display.grid(column=0, row=0)
 
-        self.Keyboard = Keyboard(self)
+        self.Keyboard = Keyboard(self, self.set_operation)
         self.Keyboard.grid(column=0, row=1)
+
+        self.selector = Selector(self.Keyboard, self.change_status)
+        self.selector.grid(column=0, row=1)
 
             
 
@@ -273,6 +276,10 @@ class Controlator(ttk.Frame):
 
         self.display.paint(self.dispValue)
 
+    def change_status(self,status):
+        self.Keyboard.status = status
+        self.reset()
+
 class Display(ttk.Frame):
 
     def __init__(self, parent):
@@ -294,26 +301,29 @@ class Display(ttk.Frame):
         self.lbl.config(text=algo)
 
 class Selector(ttk.Frame):
-    def __init__(self, parent, status="N"):
+    def __init__(self, parent, command, status="N"):
         ttk.Frame.__init__(self, parent, width=68, height=50)
         self.status = status
         self.__value = StringVar()
         self.__value.set(self.status)
+        self.command = command
     
         radiob1 = ttk.Radiobutton(self, text="N", value="N", name="rbtn_normal", variable=self.__value, command=self.__click)
         radiob1.place(x=0, y=5)
         radiob2= ttk.Radiobutton(self, text="R", value="R", name="rbtn_romano", variable=self.__value, command=self.__click)
-        radiob1.place(x=0, y=30)
+        radiob2.place(x=0, y=30)
 
     def __click(self):
         self.status = self.__value.get()
+        self.command(self.status)
            
 class Keyboard(ttk.Frame):
-    def __init__(self, parent, status="N"):
+    def __init__(self, parent, command, status="N"):
         ttk.Frame.__init__(self, parent, height=252, width=272)
         self.__status = status
         self.listaBRomanos = []
         self.listaBNormales = []
+        self.command = command
 
         if self.__status == "N":
             self.pintaNormal()
@@ -337,7 +347,7 @@ class Keyboard(ttk.Frame):
     def pintaNormal(self):
         if len(self.listaBNormales) == 0:
             for properties in normal_buttons:
-                btn = CalcButton(self, properties["text"], None, properties.get("W", 1), properties.get("H",1))
+                btn = CalcButton(self, properties["text"], self.command, properties.get("W", 1), properties.get("H",1))
                 self.listaBNormales.append((btn, properties))
                 btn.grid(column=properties["col"], row=properties["row"], columnspan=properties.get("W",1), rowspan=properties.get("H", 1))
         else:
--- test_keyboard.py
+++ test_keyboard.py
@@ -7,12 +7,12 @@ from tkinter import ttk
 
 class TestKeyboard(tkinterTestcase.tkTestCase):
     def setUp(self):
-        self.k = calculator.Keyboard(self.root)
-        self.k.pack()
-        self.k.wait_visibility()
+        self.k = calculator.Keyboard(self.root) #primero lo colgamos en la pantalla
+        self.k.pack() #lo empaquetamos
+        self.k.wait_visibility() #metodo para que lo pinte antes de probar test
     
     def tearDown(self):
-        self.k.update()
+        self.k.update() 
         self.k.destroy()
 
     def test_render_OK(self):
@@ -27,11 +27,11 @@ class TestKeyboard(tkinterTestcase.tkTestCase):
         teclado_romano.pack()
         teclado_romano.wait_visibility()
 
-        self.assertEqual(self.k.winfo_height(), 250)
-        self.assertEqual(self.k.winfo_width(), 272)
+        self.assertEqual(teclado_romano.winfo_height(), 250)
+        self.assertEqual(teclado_romano.winfo_width(), 272)
         for btn in teclado_romano.children.values():
             self.assertIsInstance(btn, calculator.CalcButton)
-        self.assertEqual(len(self.k.listaBRomanos),0)
+        self.assertEqual(len(teclado_romano.listaBRomanos),13)
 
     def test_change_staus_keyboard(self):
         self.assertEqual(self.k.status, "N")
